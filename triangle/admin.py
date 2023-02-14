944637e6cad07068d8a3408ad2eb3d80436e83d4
Pashatishinin <99612110+Pashatishinin@users.noreply.github.com>
2023-02-14 22:04:48 +0100

    Middleware

--- triangle/admin.py
+++ triangle/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, LoggerModel
+
+
+@admin.register(LoggerModel)
+class LoggerModelAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'path', 'method')
+    fieldsets =(
+    (None, {'fields': ['path', 'method']}),
+    (None, {'fields': ['os', 'host']}),
+    ("Date information", {'fields': ['timestamp']}),
+    ('Body', {'fields': ['body'], 'classes': ['collapse']}),
+    ('DATA', {'fields': ['data'], 'classes': ['collapse']}))
+
+    list_filter = ['timestamp']
+    search_fields = ['path']
+
+    date_hierarchy = 'timestamp'
+    actions_on_top = False
+    actions_on_bottom = True
+
+    list_display_links = ('path',)
+
+
+admin.site.register(Person)
